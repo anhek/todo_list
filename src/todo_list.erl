4730e4a6f7dcfa229bf4bc78ba49c60a24290772
anhek <anton.grsm@gmail.com>
2023-08-10 14:23:53 +0300

    Improve functions specs

--- src/todo_list.erl
+++ src/todo_list.erl
@@ -9,25 +9,25 @@
 %%% API
 %%%===================================================================
 
--spec get_items() -> {ok, [item()]}.
+-spec get_items() -> {ok, [item()]} | no_return().
 %% @doc Get all not deleted items
 get_items() ->
     todo_list_db:get_items().
 
 %% @doc Add item to list
 %% @param Text Text of item
--spec add_item(Text :: binary()) -> {ok, ItemId :: item_id()}.
+-spec add_item(Text :: binary()) -> {ok, ItemId :: item_id()} | no_return().
 add_item(Text) ->
     todo_list_db:add_item(Text).
 
 %% @doc Mark item as done or not done
 %% @param ItemId Item identifier
 %% @param IsDone Boolean flag
--spec mark_item(Id :: item_id(), IsDone :: boolean()) -> ok | {error, item_not_found}.
+-spec mark_item(Id :: item_id(), IsDone :: boolean()) -> ok | {error, item_not_found} | no_return().
 mark_item(ItemId, IsDone) ->
     todo_list_db:mark_item(ItemId, IsDone).
 
--spec delete_item(Id :: item_id()) -> ok | {error, item_not_found}.
+-spec delete_item(Id :: item_id()) -> ok | {error, item_not_found} | no_return().
 %% @doc Delete item from list
 %% @param ItemId Item identifier
 delete_item(ItemId) ->
--- src/todo_list_db.erl
+++ src/todo_list_db.erl
@@ -10,18 +10,18 @@
 %%% API
 %%%===================================================================
 
--spec get_items() -> {ok, [map()]}.
+-spec get_items() -> {ok, [map()]} | no_return().
 get_items() ->
     {ok, Cols, Rows} = pgapp:equery(?DB, <<"SELECT id, text, is_done FROM public.item WHERE deleted = false">>, []),
     Result = rows_to_maps(Cols, Rows),
     {ok, Result}.
 
--spec add_item(Text :: binary()) -> {ok, Id :: non_neg_integer()}.
+-spec add_item(Text :: binary()) -> {ok, Id :: non_neg_integer()} | no_return().
 add_item(Text) ->
     {ok, _, _, [{Id}]} = pgapp:equery(?DB, <<"INSERT INTO public.item (text) VALUES ($1) RETURNING id">>, [Text]),
     {ok, Id}.
 
--spec mark_item(ItemId :: non_neg_integer(), IsDone :: boolean()) -> ok | {error, item_not_found}.
+-spec mark_item(ItemId :: non_neg_integer(), IsDone :: boolean()) -> ok | {error, item_not_found} | no_return().
 mark_item(ItemId, IsDone) ->
     {ok, Count} = pgapp:equery(?DB, <<"UPDATE public.item SET is_done = $2 WHERE id = $1">>, [ItemId, IsDone]),
     case Count of
@@ -29,7 +29,7 @@ mark_item(ItemId, IsDone) ->
         0 -> {error, item_not_found}
     end.
 
--spec delete_item(ItemId :: non_neg_integer()) -> ok | {error, item_not_found}.
+-spec delete_item(ItemId :: non_neg_integer()) -> ok | {error, item_not_found} | no_return().
 delete_item(ItemId) ->
     {ok, Count} = pgapp:equery(?DB,
         <<"UPDATE public.item SET deleted = true WHERE id = $1">>, [ItemId]),
